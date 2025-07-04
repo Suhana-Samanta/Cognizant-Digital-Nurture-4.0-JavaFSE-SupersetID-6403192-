7e1e0203a3641e609c3001886750790b64299b4d
Suhana-Samanta <suhanasamanta2003@gmail.com>
2025-07-04 10:58:53 +0530

    completed

--- Week 3/Exercise 4(Creating n Configuring Maven Project)/src/main/java/com/suhana/Main.java
+++ Week 3/Exercise 4(Creating n Configuring Maven Project)/src/main/java/com/suhana/Main.java
@@ -1,12 +1,15 @@
-package com.sudip;
+package com.suhana;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
+
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-
+        System.out.println();
         System.out.println("Spring Context Initialized!");
+        System.out.println();
         context.close();
     }
+
 }
